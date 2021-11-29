05d87f80447704f8c465d5faf0e5e99036b5b268
harshpagar <72097667+harshpagar@users.noreply.github.com>
2021-11-29 16:52:01 +0530

    .
    add

--- eval_coding_1/app.js
+++ eval_coding_1/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 12345;
 
+app.post("/",(req,res)=>{
+    res.send("Hi");
+});
+app.listen(port,() =>{
+    console.log(`connection is setup at ${port}`);
+})
