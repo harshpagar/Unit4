901cc989d1c216d678e78b22d4170e598eca9745
harshpagar <72097667+harshpagar@users.noreply.github.com>
2021-12-18 23:24:27 +0530

    add
    add

---
+++ Movie_app/app.js
@@ -0,0 +1,89 @@
+const express =  require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+
+const connect = () => { 
+    return mongoose.connect("mongodb://127.0.0.1:27017/movie",{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+}
+
+const movieSchema = new mongoose.Schema({
+    movie_name: {type: String},
+    movie_genre: {type: String},
+    production_year: {type: Number},  
+    budget: {type: Number}
+},{
+    versionKey: false,
+    timestamps:true
+});
+
+const Movie = mongoose.model("Movie",movieSchema);
+
+
+app.get("/movies",async (req, res)=>{
+    try{
+        const movies =await Movie.find({}).lean().exec();
+        res.status(200).send(movies);
+    }
+    catch(e){
+        res.send(e);
+    }
+    
+})
+
+
+app.post("/movies", async (req, res) => {
+    console.log(req.body)
+   const movie = await Movie.create(req.body);
+   res.status(201).send(movie);
+})
+
+
+app.get("/movies/:id", async (req, res) => {
+    try{
+        const movie = await Movie.findById(req.params.id);
+        res.status(201).send(movie);
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
+})
+
+
+
+app.patch("/movies/:id", async (req, res) => {
+    try{
+        const updated_movie = await Movie.findByIdAndUpdate(req.params.id, req.body,{new:true});
+        res.send(updated_movie);
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
+});
+
+
+
+app.delete("/movies/:id", async (req, res) => {
+    try{
+        const movie = await Movie.findByIdAndDelete(req.params.id);
+        res.send(`${movie} deleted sucessfuly`)
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
+})
+
+const start =async () => {
+    await connect();
+
+    app.listen(3000,()=>{
+        console.log("listning port 3000");
+    })
+}
+
+start();
